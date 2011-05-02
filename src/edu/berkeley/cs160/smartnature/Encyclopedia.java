ac13eeed6aabf7ea5a7c71900490ea76e1d663a2
Kevin Liou <kwliou@hotmail.com>
2011-05-02 00:32:48 -0700

    fixed encyclopedia issues

--- src/edu/berkeley/cs160/smartnature/Encyclopedia.java
+++ src/edu/berkeley/cs160/smartnature/Encyclopedia.java
@@ -1,8 +1,5 @@
 package edu.berkeley.cs160.smartnature;
 
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
 
 import org.jsoup.Jsoup;
@@ -13,12 +10,11 @@ import org.jsoup.select.Elements;
 import android.app.ListActivity;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -27,130 +23,99 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class Encyclopedia extends ListActivity implements View.OnClickListener, AdapterView.OnItemClickListener{
+public class Encyclopedia extends ListActivity implements View.OnClickListener, AdapterView.OnItemClickListener, Runnable {
 	
-	static ResultAdapter adapter;
-
-
+	ResultAdapter adapter;
+	ArrayList<SearchResult> resultList = new ArrayList<SearchResult>();
 	String pName = "";
 	String name = "";
 	EditText search;
-
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-    		Bundle extras = getIntent().getExtras();
-    		if (extras != null && extras.containsKey("name")) {
-    			name = extras.getString("name");
-    			setTitle(name);
-    		}
-    		
-    		
-        setContentView(R.layout.encycl);
-        
-        
-        getListView().setOnItemClickListener(this);
-		
-		Button searchButton = (Button) findViewById(R.id.searchButton);
-		search = (EditText)findViewById(R.id.searchText);
-		search.setText(name);
-		
-		searchButton.setOnClickListener(new View.OnClickListener() {
-        	public void onClick (View v) {
-        		
-        		EditText search = (EditText)findViewById(R.id.searchText);
-
-        		if(!search.getText().toString().equals("")){
-	        		String searchURL = "http://www.plantcare.com/encyclopedia/search.aspx?q=" + search.getText().toString();
-	        		//LinearLayout content = (LinearLayout) findViewById(R.id.content);
-					//content.removeAllViews();
-	        		
-	        		try {
-	        			
-	        			ArrayList <SearchResult> resultList = new ArrayList <SearchResult> ();
-						Document doc = Jsoup.connect(searchURL).get();
-						Element resultBox = doc.getElementById("searchEncyclopedia");
-						if(resultBox.child(1).attr("id").equals("_ctl0_mainHolder_noresults")){
-							//Toast.makeText(Encyclopedia.this, "Sorry, no results were found.", Toast.LENGTH_SHORT).show();
-							resultList.add(new SearchResult ("No results found", "Please try refining your search", "", ""));
-						}
-						else{
-							final Elements results = resultBox.child(1).children();
-						
-							int numResults = results.size();
-							for(int i = 0; i < numResults; i++){
-								
-								final Element next = results.first();
-								String plantURL = "http://www.plantcare.com" + next.child(0).child(0).child(0).attr("src");
-								String name = next.child(1).text();
-								String altNames = next.child(2).text();
-								String linkURL = "http://www.plantcare.com/encyclopedia/" + next.child(1).attr("href");
-								resultList.add(new SearchResult (name, altNames, plantURL, linkURL));
-								
-								results.remove(0);
-								
-							}
-							//ListView listView = (ListView) findViewById(R.id.searchList);
-		
-							
-							
-						}
-						adapter = new ResultAdapter(Encyclopedia.this, R.layout.search_item, resultList);
-						setListAdapter(adapter);
-						
-						} catch (IOException e) {
-						
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-        		}
-        		else{
-        			Toast.makeText(Encyclopedia.this, "Please enter the plant you are looking for.", Toast.LENGTH_SHORT).show();
-        		}
-				//Intent browserIntent = new Intent("android.intent.action.VIEW", Uri.parse(searchURL));
-        		//startActivity(browserIntent);
-        		
-
-        	
-        	}
-
-			
-		});
-        
-    }
-    
+	
+	/** Called when the activity is first created. */
 	@Override
-	public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
-		
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS); // Window.FEATURE_PROGRESS
+		setContentView(R.layout.encycl);
+		adapter = new ResultAdapter(this, R.layout.search_list_item, resultList);
+		setListAdapter(adapter);
+		getListView().setOnItemClickListener(this);
+		((Button) findViewById(R.id.searchButton)).setOnClickListener(this);
+		search = (EditText) findViewById(R.id.searchText);
+		search.setText(name);
+	}
+	
+	@Override
+	public void run() {
+		String searchText = ((EditText) findViewById(R.id.searchText)).getText().toString();
+		String searchURL = "http://www.plantcare.com/encyclopedia/search.aspx?q=" + searchText;
+		Element resultBox = null;
+		try {
+			Document doc = Jsoup.connect(searchURL).get();
+			resultBox = doc.getElementById("searchEncyclopedia");
+		} catch (Exception e) {
+			e.printStackTrace();
+			runOnUiThread(new Runnable() {
+				@Override public void run() {
+					setProgressBarIndeterminateVisibility(false);
+					findViewById(R.id.encycl_msg).setVisibility(View.VISIBLE);
+				}
+			});
+			return;
+		}
+		resultList.clear();
+		invalidate();
+		if (resultBox.child(1).attr("id").equals("_ctl0_mainHolder_noresults")) {
+			// Toast.makeText(Encyclopedia.this, "Sorry, no results were found.", Toast.LENGTH_SHORT).show();
+			resultList.add(new SearchResult("No results found", "Please try refining your search", "", ""));
+			invalidate();
+		} else {
+			Elements results = resultBox.child(1).children();
+			for (int i = 0; i < results.size(); i++) {
+				Element next = results.get(i);
+				String plantURL = "http://www.plantcare.com" + next.child(0).child(0).child(0).attr("src");
+				String name = next.child(1).text();
+				String altNames = next.child(2).text().replaceFirst("Also known as:", "Known as:");
+				String linkURL = "http://www.plantcare.com/encyclopedia/" + next.child(1).attr("href");
+				resultList.add(new SearchResult(name, altNames, plantURL, linkURL));
+				invalidate();
+			}
+		}
+		runOnUiThread(new Runnable() {
+			@Override public void run() { setProgressBarIndeterminateVisibility(false); }
+		});
+	}
+	
+	public void invalidate() {
+		runOnUiThread(new Runnable() {
+			@Override public void run() { adapter.notifyDataSetChanged(); }
+		});
+	}
+	
+	@Override
+	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 		Intent result = new Intent(Encyclopedia.this, EncyclopediaResult.class);
-		Bundle bundle = new Bundle();
-		
-		String plantURL = ((TextView)arg1.findViewById(R.id.linkURL)).getText().toString();
-		pName = ((TextView)arg1.findViewById(R.id.name)).getText().toString();
-		
-		
-		bundle.putString("name", pName);
-		bundle.putString("linkURL", plantURL);
-		
-		result.putExtras(bundle);
-		
+		String plantURL = ((TextView) view.findViewById(R.id.linkURL)).getText().toString();
+		pName = ((TextView) view.findViewById(R.id.name)).getText().toString();
+		result.putExtra("name", pName);
+		result.putExtra("linkURL", plantURL);
 		startActivity(result);
 	}
 	
-	
-
 	@Override
-	public void onClick(View arg0) {
-		
-		String plantURL = ((TextView)arg0.findViewById(R.id.linkURL)).getText().toString();
-		Intent browserIntent = new Intent("android.intent.action.VIEW", Uri.parse(plantURL));
-		startActivity(browserIntent);
-		
+	public void onClick(View view) {
+		String searchText = ((EditText) findViewById(R.id.searchText)).getText().toString();
+		if (searchText.equals(""))
+			Toast.makeText(this, "Please enter the plant you are looking for.", Toast.LENGTH_SHORT).show();
+		else {
+			findViewById(R.id.encycl_msg).setVisibility(View.GONE);
+			setProgressBarIndeterminateVisibility(true);
+			new Thread(this).start();
+		}
 	}
 	
-    class ResultAdapter extends ArrayAdapter<SearchResult> {
+	class ResultAdapter extends ArrayAdapter<SearchResult> {
+		
 		private ArrayList<SearchResult> items;
 		private LayoutInflater li;
 		
@@ -159,24 +124,20 @@ public class Encyclopedia extends ListActivity implements View.OnClickListener, 
 			li = ((ListActivity) context).getLayoutInflater();
 			this.items = items;
 		}
-
+		
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent) {
 			View v = convertView;
 			if (v == null)
-				v = li.inflate(R.layout.search_item, null);
+				v = li.inflate(R.layout.search_list_item, null);
 			SearchResult s = items.get(position);
 			((TextView) v.findViewById(R.id.name)).setText(s.getName());
 			((TextView) v.findViewById(R.id.altNames)).setText(s.getAltNames());
-			if(!s.getLinkURL().equals("")){
+			if (!s.getLinkURL().equals("")) {
 				((TextView) v.findViewById(R.id.linkURL)).setText(s.getLinkURL());
 				try {
-					((ImageView) v.findViewById(R.id.searchPic)).setImageBitmap(BitmapFactory.decodeStream((new URL(s.getPicURL())).openConnection().getInputStream()));
-				} catch (MalformedURLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
+					((ImageView) v.findViewById(R.id.searchPic)).setImageBitmap(s.getBitmap());
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
@@ -184,6 +145,5 @@ public class Encyclopedia extends ListActivity implements View.OnClickListener, 
 			return v;
 		}
 	}
-
 
 }
--- src/edu/berkeley/cs160/smartnature/SearchResult.java
+++ src/edu/berkeley/cs160/smartnature/SearchResult.java
@@ -1,18 +1,27 @@
 package edu.berkeley.cs160.smartnature;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import java.io.IOException;
+import java.net.URL;
+
 public class SearchResult {
 
 	private String name;
 	private String altNames;
 	private String picURL;
 	private String linkURL;
+	private Bitmap bmp;
 	
 	SearchResult(String n, String a, String p, String l){
 		name = n;
 		altNames = a;
 		picURL = p;
 		linkURL = l;
-		
+		try {
+			bmp = BitmapFactory.decodeStream(new URL(picURL).openConnection().getInputStream());
+		} catch (IOException e) { e.printStackTrace(); }
 	}
 	
 	public String getName(){
@@ -25,6 +34,9 @@ public class SearchResult {
 		return picURL;
 	}
 	
+	public Bitmap getBitmap() {
+		return bmp;
+	}
 	public String getLinkURL(){
 		return linkURL;
 	}
