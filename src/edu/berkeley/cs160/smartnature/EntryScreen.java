928017caedd2d2734fa9bb0561c606ad097b1eb8
benchu <ben8043@gmail.com>
2011-05-04 02:43:45 -0700

    Voice recognition on the EntryScreen

--- src/edu/berkeley/cs160/smartnature/EntryScreen.java
+++ src/edu/berkeley/cs160/smartnature/EntryScreen.java
@@ -1,8 +1,15 @@
 package edu.berkeley.cs160.smartnature;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.ListActivity;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -12,7 +19,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class EntryScreen extends ListActivity {
+public class EntryScreen extends ListActivity implements View.OnClickListener{
 
 	Garden garden;
 	Plot plot;
@@ -21,8 +28,13 @@ public class EntryScreen extends ListActivity {
 	int gardenID, plotID, plantID, entryID, pa_pk; 
 
 	TextView entryDate, entryText;
-	Button editEntryButton, deleteEntryButton;
+	Button editEntryButton, deleteEntryButton, speakButton;
 	EditText newJournalText;
+	
+	/** Voice Recognition */
+	private static final int VOICE_RECOGNITION_REQUEST_CODE = 1234;
+	ArrayList<String> matches = new ArrayList<String>();
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -73,7 +85,49 @@ public class EntryScreen extends ListActivity {
       }
     });
 		
+		/** Voice Recognition **/
+    speakButton = (Button) findViewById(R.id.recordVoiceButton2);
+    
+    PackageManager pm = getPackageManager();
+    List<ResolveInfo> activities = pm.queryIntentActivities(
+            new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH), 0);
+    if (activities.size() != 0) {
+        speakButton.setOnClickListener(this);
+    } else {
+        //speakButton.setEnabled(false);
+        //speakButton.setText("Recognizer not present");
+        speakButton.setVisibility(View.GONE);
+    }
+		
 	}
+	
+  /** Handle the click on the start recognition button. */
+	@Override
+	public void onClick(View v) {
+    if (v.getId() == R.id.recordVoiceButton2) {
+      startVoiceRecognitionActivity();
+    }
+	}
+
+  /** Fire an intent to start the speech recognition activity. */
+  private void startVoiceRecognitionActivity() {
+      Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+      intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+              RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+      intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speech recognition demo");
+      startActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE);
+  }
+
+  /** Handle the results from the recognition activity. */
+  @Override
+  protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+      if (requestCode == VOICE_RECOGNITION_REQUEST_CODE && resultCode == RESULT_OK) {
+      		matches = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);  
+  				EditText entry = (EditText) findViewById(R.id.entryText);  				
+					entry.setText(entry.getText() + matches.get(0).toString());
+      }
+      super.onActivityResult(requestCode, resultCode, data);
+  }
 
 
 	@Override
